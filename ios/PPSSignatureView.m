a9b0579772121e5c9dec1fae82ff2cffe085c03b
nakklam <teenueng@gmail.com>
2016-09-19 13:17:36 +0700

    Update PPSSignatureView.m

--- ios/PPSSignatureView.m
+++ ios/PPSSignatureView.m
@@ -189,17 +189,6 @@ static PPSSignaturePoint ViewPointToGL(CGPoint viewPoint, CGRect bounds, GLKVect
 	glClear(GL_COLOR_BUFFER_BIT);
 	
 	[effect prepareToDraw];
-	
-	// Drawing of signature lines
-	if (length > 2) {
-		glBindVertexArrayOES(vertexArray);
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, length);
-	}
-	
-	if (dotsLength > 0) {
-		glBindVertexArrayOES(dotsArray);
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, dotsLength);
-	}
 }
 
 
